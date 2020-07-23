637f1f6b800d1b48065db09afcac4259b73b8d1f
berkaykarakoc <karakocberkay@outlook.com>
2020-07-23 12:14:21 +0300

    small changes

--- services/api/connections/database-connection.js
+++ services/api/connections/database-connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(`mongodb://localhost:27017/${process.env.DEV_DATABASE}`, {
+mongoose.connect(`mongodb://${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
@@ -9,7 +9,7 @@ const conn = mongoose.connection;
 
 conn.on("error", (error) => console.log("Cannot connect to the MongoDB"));
 conn.once("open", () =>
-    console.log(`Connected to ${process.env.DEV_DATABASE}`)
+    console.log(`Connected to ${process.env.DATABASE_NAME}`)
 );
 
 module.exports = { conn };
