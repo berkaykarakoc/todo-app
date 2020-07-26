71ab9650008ed48a2590a472695eaf09fb881847
berkaykarakoc <karakocberkay@outlook.com>
2020-07-26 19:16:29 +0300

    fix

--- services/web/wait.sh
+++ services/web/wait.sh
@@ -3,7 +3,7 @@
 echo "Waiting for localhost:3000..."
 for i in 1 2 3 4 5; do
     echo $i
-    if [! nc -z localhost 3000]; then
+    if [ ! nc -z localhost 3000 ]; then
         sleep 2
     fi
 done
